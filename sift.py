d4dd70fc6a916ec28c3cb5fd5b1b55f61a8c648a
egiacomazzi <egiacomazzi@uos.de>
2017-11-10 18:48:55 +0000

    add hologram

--- sift.py
+++ sift.py
@@ -35,13 +35,40 @@ def get_descriptor_sift(sift, img):
 def bfMatching_SIFT(bf, kp_img1, kp_img2, des_img1, des_img2, img1, img2):
     matches = bf.knnMatch(des_img1, des_img2, k=2)
     good = []
+    good2 = []
     for m,n in matches:
         if m.distance < 0.75*n.distance:
             good.append([m])
+            good2.append(m)
 
     img_match = cv2.drawMatchesKnn(img1, kp_img1, img2, kp_img2, good, img1, flags=2)
     cv2.imwrite('matching_SIFT_BFM.jpg',img_match)
-    #return matches
+
+    MIN_MATCH_COUNT = 10
+    if len(good)>=MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp_img1[m.queryIdx].pt for m in good2 ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp_img2[m.trainIdx].pt for m in good2 ]).reshape(-1,1,2)
+
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+
+            h,w, = img1.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv2.perspectiveTransform(pts,M)
+
+            img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+    else:
+        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        matchesMask = None
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                singlePointColor = None,
+                matchesMask = matchesMask, # draw only inliers
+                flags = 2)
+
+    img3 = cv2.drawMatches(img1, kp_img1, img2, kp_img2, good2, None, **draw_params)
+    cv2.imwrite('hologram.jpg',img3)
+    #return good
 
 ############################### ORB ##############################################################
 '''
@@ -78,6 +105,34 @@ def matching_FLANN(flann, kp_img1, kp_img2, des_img1, des_img2, img1, img2):
     img4 = cv2.drawMatchesKnn(img1,kp_img1,img2,kp_img2,matches,None,**draw_params)
     cv2.imwrite('matching_FLANN.jpg',img4)
     #return matches
+
+def hologram(good,kp_img1, kp_img2, img1, img2):
+    MIN_MATCH_COUNT = 10
+    if len(good)>MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp_img1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp_img2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+
+            h,w,d = img1.shape
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv2.perspectiveTransform(pts,M)
+
+            img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+    else:
+        print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+        matchesMask = None
+    draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                singlePointColor = None,
+                matchesMask = matchesMask, # draw only inliers
+                flags = 2)
+
+    img3 = cv2.drawMatches(img1,kp_img1,img2,kp_img2,good,None,**draw_params)
+    cv2.imwrite('hologram.jpg',img3)
+
+
 ############################### Draw Keypoints ###############################
 def draw_keypoints(kp, img):
     img = cv2.drawKeypoints(img,kp,img, flags=cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
@@ -104,20 +159,25 @@ def main():
     flann_ORB = cv2.FlannBasedMatcher(index_params_ORB,search_params_ORB)
 
     #import images
-    img_original = cv2.imread('img.png',0)          # queryImage
-    img_house = cv2.imread('white_house.jpg',0) # trainImage
+    nachttisch_tasse = cv2.imread('ricola_laptop.jpg',0)          # queryImage
+    tasse = cv2.imread('smarti_tisch.jpg',0) # trainImage
 
-    tasse = cv2.imread('tasse_c.JPG',0)
-    nachttisch_tasse = cv2.imread('nachttisch_tasse_c.JPG',0)
-    schreibtisch = cv2.imread('schreibtisch_c.JPG',0)
+    kp_tasse, des_tasse = get_descriptor_sift(sift, tasse)
+    kp_nachttisch, des_nachttisch = get_descriptor_sift(sift, nachttisch_tasse)
+    bfMatching_SIFT(bf_sift, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
+
+
+    # tasse = cv2.imread('tasse_c.JPG',0)
+    # nachttisch_tasse = cv2.imread('nachttisch_tasse_c.JPG',0)
+    # schreibtisch = cv2.imread('schreibtisch_c.JPG',0)
     #save_pic(schreibtisch)
 
     # detect tasse on nachttisch
     ###### SIFT BFM #####
     # kp_tasse, des_tasse = get_descriptor_sift(sift, tasse)
     # kp_nachttisch, des_nachttisch = get_descriptor_sift(sift, nachttisch_tasse)
-    # matches = bfMatching_SIFT(bf_sift, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
-
+    # bfMatching_SIFT(bf_sift, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
+    #hologram(good,kp_tasse, kp_nachttisch,tasse, nachttisch_tasse)
     ###### SIFT FLANN #####
     #matching_FLANN(flann_SIFT, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
 
@@ -125,10 +185,12 @@ def main():
     #draw_keypoints(kp_nachttisch,nachttisch_tasse)
 
     ##### ORB BFM #####
-    kp_tasse, des_tasse = get_key_des_ORB(orb, tasse)
-    kp_nachttisch, des_nachttisch = get_key_des_ORB(orb, nachttisch_tasse)
-    matching_BFM_ORB(bf_ORB, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
+    # kp_tasse, des_tasse = get_key_des_ORB(orb, tasse)
+    # kp_nachttisch, des_nachttisch = get_key_des_ORB(orb, nachttisch_tasse)
+    # matching_BFM_ORB(bf_ORB, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
+    #
+    # ###### ORB FLANN #####
+    # matching_FLANN(flann_ORB, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
 
-    ###### ORB FLANN #####
-    matching_FLANN(flann_ORB, kp_tasse, kp_nachttisch, des_tasse, des_nachttisch, tasse, nachttisch_tasse)
+
 main()
